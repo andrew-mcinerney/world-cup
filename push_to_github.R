ec676b0f09ba45d8b784a07d206d0036bd4759a6
andrew-mcinerney <andrew.mcinerney@ul.ie>
2022-11-21 18:58:10 +0000

    commit from Rstudio

---
+++ push_to_github.R
@@ -0,0 +1,52 @@
+library(git2r)
+
+gitcommit <- function(msg = "commit from Rstudio", dir = getwd()){
+  cmd = sprintf("git commit -m\"%s\"",msg)
+  system(cmd)
+}
+
+# Git status.
+gitstatus <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git status"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+# Git add.
+gitadd <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git add --all"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+
+# Git push.
+gitpush <- function(dir = getwd()){
+  cmd_list <- list(
+    cmd1 = tolower(substr(dir,1,2)),
+    cmd2 = paste("cd",dir),
+    cmd3 = "git push"
+  )
+  cmd <- paste(unlist(cmd_list),collapse = " & ")
+  shell(cmd)
+}
+
+git2r::config(user.name = "andrew-mcinerney", user.email = "andrew.mcinerney@ul.ie")
+
+# Check git status.
+gitstatus()
+
+# Add and commit changes. 
+gitadd()
+gitcommit()
+
+# Push changes to github.
+gitpush()
